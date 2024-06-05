041df32a48f7ac5db3fe5421cfb82ce3c7f00b43
leyvameister <140881301+leyvameister@users.noreply.github.com>
2024-06-04 21:33:34 -0700

    Update deleteProductFromDB.js

--- js/deleteProductFromDB.js
+++ js/deleteProductFromDB.js
@@ -2,15 +2,10 @@ export async function deleteProduct(productCard) {
     const productId = productCard.getAttribute("data-product-id");
 
     try {
-        const response = await fetch(`https://one-alurageek-json-server.vercel.app/products/${productId}`, {
+        await fetch(`https://one-alurageek-json-server.vercel.app/products/${productId}`, {
             method: 'DELETE'
         });
-
-        if (response.ok) {
             productCard.remove();
-        } else {
-            console.error('Error al eliminar el producto');
-        }
     } catch (error) {
         console.error('Error:', error);
     }
