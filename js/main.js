c9e666c951cb5b260c2e33c5b37e69b0e5ffa5c7
leyvameister <140881301+leyvameister@users.noreply.github.com>
2024-06-04 21:03:51 -0700

    Update

--- js/main.js
+++ js/main.js
@@ -2,17 +2,6 @@ import { renderProducts } from "./renderProducts.js";
 import { submitProduct } from "./formSubmitHandler.js";
 import { registerEventListener } from "./registerFieldEventListener.js";
 
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
-
 renderProducts();
 
 document.querySelector("[data-form]").addEventListener("submit", submitProduct);
